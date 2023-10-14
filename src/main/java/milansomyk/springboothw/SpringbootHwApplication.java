40b8a6590b2a8c484b2f50a9d7230bd2a2dced55
fabmansik <milansomyk@gmail.com>
2023-10-14 17:28:58 +0300

    springboot initial commit

---
+++ src/main/java/milansomyk/springboothw/SpringbootHwApplication.java
@@ -0,0 +1,13 @@
+package milansomyk.springboothw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/milansomyk/springboothw/SpringbootHwApplicationTests.java
@@ -0,0 +1,13 @@
+package milansomyk.springboothw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
