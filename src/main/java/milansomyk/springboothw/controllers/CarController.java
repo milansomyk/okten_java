be2eea66d4d8a94e427451c2e815ee81cea84ebf
fabmansik <milansomyk@gmail.com>
2023-10-19 11:14:46 +0300

    hw9 in progress...

--- src/main/java/milansomyk/springboothw/controllers/CarController.java
+++ src/main/java/milansomyk/springboothw/controllers/CarController.java
@@ -1,5 +1,6 @@
 package milansomyk.springboothw.controllers;
 
+import jakarta.validation.Valid;
 import jakarta.websocket.server.PathParam;
 import lombok.RequiredArgsConstructor;
 import milansomyk.springboothw.dto.CarDto;
@@ -13,6 +14,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/cars")
+
 public class CarController {
     private final CarService carService;
 
@@ -23,7 +25,7 @@ public class CarController {
 
 
     @PostMapping
-    public ResponseEntity<CarDto> create(@RequestBody CarDto carDto){
+    public ResponseEntity<CarDto> create(@RequestBody @Valid CarDto carDto){
         return ResponseEntity.status(HttpStatus.CREATED).body(carService.create(carDto));
     }
 
